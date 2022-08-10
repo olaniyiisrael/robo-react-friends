fde5250be2e0730bc1e2bc9a3203ac2bd8b36033
olaniyiisrael <davdto56@gmail.com>
2022-08-10 15:58:07 +0100

    robo-react-friends

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import SearchBox from './SearchBox';
+import Cardlist from './Cardlist';
+import users from './users.json';
+import "./App.css";
+import React from 'react';
+
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      searchInput: ''
+    }
+  }
+
+  updateSearchInput = (event) =>{
+    this.setState({searchInput:event.target.value})
+  }
+
+  render(){
+    const filteredRobots = users.filter (item =>(item.name.toLowerCase().includes(this.state.searchInput.toLowerCase())))
+
+
+    return (
+      <div className="">
+        <h1 style={{textAlign:'center'}}>Robofriends</h1>
+        
+        <SearchBox updateSearchInput={this.updateSearchInput}/>
+        <Cardlist clients ={filteredRobots}/>
+      </div>
+    );
+  }
+}
+
+
+export default App;
